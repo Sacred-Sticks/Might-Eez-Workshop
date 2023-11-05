345f533b3ad5f338de85bf7dd7833b763bf4f4eb
Lucas Ackman <AckmanLD@gmail.com>
2023-11-05 15:18:02 -0600

    Update WorkstationEditor.cs

--- Lets Not Focus on That Right Now/Assets/_Project/Scripts/Toy Production/Workstations/Editor/WorkstationEditor.cs
+++ Lets Not Focus on That Right Now/Assets/_Project/Scripts/Toy Production/Workstations/Editor/WorkstationEditor.cs
@@ -1,54 +1,54 @@
-﻿using UnityEditor;
-using UnityEditor.Rendering;
+﻿using System.Collections.Generic;
+using UnityEditor;
 
 [CustomEditor(typeof(Workstation))]
 public class WorkstationEditor : Editor
 {
+    private SerializedProperty m_Script;
     private SerializedProperty workstationType;
-
-    private const string toyPartPropertyName = "toyPart";
-    private const string numToyPartsPropertyName = "numToyParts";
+    private SerializedProperty materialType;
+    private SerializedProperty toyPart;
+    private SerializedProperty numToyParts;
 
     private void OnEnable()
     {
+        m_Script = serializedObject.FindProperty(nameof(m_Script));
         workstationType = serializedObject.FindProperty(nameof(workstationType));
+        materialType = serializedObject.FindProperty(nameof(materialType));
+        toyPart = serializedObject.FindProperty(nameof(toyPart));
+        numToyParts = serializedObject.FindProperty(nameof(numToyParts));
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        var prop = serializedObject.GetIterator();
-        if (prop.NextVisible(true))
+        var workstation = (Workstation)target;
+
+        var desiredProperties = new List<SerializedProperty>
         {
-            EditorGUILayout.PropertyField(prop, true);
-            prop.NextVisible(true);
-        }
+            m_Script,
+            workstationType,
+        };
 
-
-        EditorGUILayout.PropertyField(workstationType);
-
-        var myComponent = (Workstation)target;
-
-        bool enterChildren = true;
-        while (prop.NextVisible(enterChildren))
+        switch (workstation.WorkstationType)
         {
-            if (prop.name != "m_Script" && prop.name != nameof(workstationType) && prop.name != toyPartPropertyName && prop.name != numToyPartsPropertyName)
-            {
-                EditorGUILayout.PropertyField(prop, true);
-            }
-            enterChildren = false;
-        }
-
-        switch (myComponent.WorkstationType)
-        {
+            case Workstation.WorkstationCategory.Dispenser:
+                desiredProperties.Add(materialType);
+                break;
+            case Workstation.WorkstationCategory.Processor:
+                desiredProperties.Add(materialType);
+                break;
             case Workstation.WorkstationCategory.Molder:
-                EditorGUILayout.PropertyField(serializedObject.FindProperty(toyPartPropertyName));
+                desiredProperties.Add(toyPart);
                 break;
             case Workstation.WorkstationCategory.Assembler:
-                EditorGUILayout.PropertyField(serializedObject.FindProperty(numToyPartsPropertyName));
+                desiredProperties.Add(numToyParts);
+                break;
+            case Workstation.WorkstationCategory.Output:
                 break;
         }
-
+        
+        desiredProperties.ForEach(p => EditorGUILayout.PropertyField(p, true));
         serializedObject.ApplyModifiedProperties();
     }
 }
